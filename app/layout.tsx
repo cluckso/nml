a415ff220478f0535636892a06a6f7bcc0381080
Steven <ststeinhoff80@gmail.com>
2026-02-23 23:35:37 -0600

    add pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import Link from "next/link"
+import Script from "next/script"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Providers } from "./providers"
@@ -23,6 +24,34 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`${inter.className} flex min-h-screen flex-col bg-background text-foreground`}>
+        {/* Meta Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2020653371834861');
+fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height={1}
+            width={1}
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2020653371834861&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <Providers>
           <Nav />
           <main className="flex-1">{children}</main>
